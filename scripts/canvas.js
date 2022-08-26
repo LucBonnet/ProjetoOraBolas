cbaa6684b01d4fd6fb8184d7cd31d36703c38214
LucBonnet <lucbonnet10@gmail.com>
2022-08-26 16:06:35 -0300

    mobile first

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -8,64 +8,64 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 let graus = 0;
 async function trajetoria() {
-  bola.x = bola.trajetoria.x[simulacao.t];
-  bola.y = bola.trajetoria.y[simulacao.t];
+    bola.x = bola.trajetoria.x[simulacao.t];
+    bola.y = bola.trajetoria.y[simulacao.t];
 
-  robo.x = robo.trajetoria.x[simulacao.t].toFixed(3);
-  robo.y = robo.trajetoria.y[simulacao.t].toFixed(3);
+    robo.x = robo.trajetoria.x[simulacao.t].toFixed(3);
+    robo.y = robo.trajetoria.y[simulacao.t].toFixed(3);
 
-  const dx = Math.abs(robo.x - bola.x).toFixed(2);
-  const dy = Math.abs(robo.y - bola.y).toFixed(2);
-  let graus = Math.atan(dy / dx) * (180 / Math.PI);
+    const dx = Math.abs(robo.x - bola.x).toFixed(2);
+    const dy = Math.abs(robo.y - bola.y).toFixed(2);
+    let graus = Math.atan(dy / dx) * (180 / Math.PI);
 
-  if (robo.x > bola.x) {
-    graus = 180 - graus;
-  }
+    if (robo.x > bola.x) {
+        graus = 180 - graus;
+    }
 
-  if (robo.y < bola.y) {
-    graus = -graus;
-  }
+    if (robo.y < bola.y) {
+        graus = -graus;
+    }
 
-  robo.graus = graus;
+    robo.graus = graus;
 
-  atualizaDados();
+    atualizaDados();
 
-  apontaRobo(robo.graus, imgRobo, robo.raio, robo.x, robo.y);
-  desenhaImagem(imgBola, bola.raio, bola.x, bola.y);
+    apontaRobo(robo.graus, imgRobo, robo.raio, robo.x, robo.y);
+    desenhaImagem(imgBola, bola.raio, bola.x, bola.y);
 
-  if (simulacao.t == 0) {
-    await delay(1000);
-  }
+    if (simulacao.t == 0) {
+        await delay(1000);
+    }
 
-  if (
-    (simulacao.t == simulacao.tFinal && simulacao.t != 0) ||
-    simulacao.t == bola.trajetoria.t.length
-  ) {
-    await delay(1000);
+    if (
+        (simulacao.t == simulacao.tFinal && simulacao.t != 0) ||
+        simulacao.t == bola.trajetoria.t.length
+    ) {
+        await delay(1000);
 
-    simulacao.t = 0;
-    resetCanvas();
-  } else {
-    simulacao.t++;
-    bola.t = bola.trajetoria.t[simulacao.t];
-  }
+        simulacao.t = 0;
+        resetCanvas();
+    } else {
+        simulacao.t++;
+        bola.t = bola.trajetoria.t[simulacao.t];
+    }
 
-  setTimeout(trajetoria, 20);
+    setTimeout(trajetoria, 20);
 }
 
 async function main() {
-  setTimeout(trajetoria, 20);
+    setTimeout(trajetoria, 20);
 }
 
 function atualizaDados() {
-  pInst.innerText = `Instante: ${bola.t}s`;
-  pPosBola.innerText = `Posição da Bola:\n(${bola.x}, ${bola.y})`;
-  pPosRobo.innerText = `Posição do Robô:\n(${robo.x}, ${robo.y})`;
+    pInst.innerText = `Instante:\n${bola.t}s`;
+    pPosBola.innerText = `Posição da Bola:\n(${bola.x}, ${bola.y})`;
+    pPosRobo.innerText = `Posição do Robô:\n(${robo.x}, ${robo.y})`;
 
-  pVelRobo.innerText = `Velocidade do Robô: ${robo.trajetoria.v[
-    simulacao.t
-  ].toFixed(3)}m/s`;
+    pVelRobo.innerText = `Velocidade do Robô:\n ${robo.trajetoria.v[
+        simulacao.t
+    ].toFixed(3)}m/s`;
 
-  const d = simulacao.d[simulacao.t];
-  pDist.innerText = `Distância:\n${d.toFixed(3)}m`;
+    const d = simulacao.d[simulacao.t];
+    pDist.innerText = `Distância:\n${d.toFixed(3)}m`;
 }
--- scripts/canvasStyle.js
+++ scripts/canvasStyle.js
@@ -11,71 +11,71 @@ imgBola.onload = () => desenhaCampo();
 imgRobo.onload = () => desenhaCampo();
 
 function desenhaCampo() {
-  ctx.fillStyle = "#51bb26";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#51bb26";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.strokeStyle = "#fff";
-  ctx.lineWidth = 25 / 2;
-  ctx.beginPath();
-  ctx.moveTo(canvas.width / 2, 0);
-  ctx.lineTo(canvas.width / 2, canvas.height);
-  ctx.stroke();
+    ctx.strokeStyle = "#fff";
+    ctx.lineWidth = 25 / 2;
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
 
-  ctx.strokeRect(-20, canvas.height / 3, 350, canvas.height / 3);
-  ctx.strokeRect(
-    canvas.width - 350 + 20,
-    canvas.height / 3,
-    500,
-    canvas.height / 3
-  );
-  ctx.closePath();
+    ctx.strokeRect(-20, canvas.height / 3, 250, canvas.height / 3);
+    ctx.strokeRect(
+        canvas.width - 250 + 20,
+        canvas.height / 3,
+        250,
+        canvas.height / 3
+    );
+    ctx.closePath();
 
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2, canvas.height / 2, 350, 0, Math.PI * 2);
-  ctx.stroke();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 250, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.closePath();
 
-  ctx.beginPath();
-  ctx.fillStyle = "#fff";
-  ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "#fff";
+    ctx.arc(canvas.width / 2, canvas.height / 2, 25, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
 }
 
 function resetCanvas() {
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  desenhaCampo();
+    desenhaCampo();
 }
 
 function desenhaImagem(img, r, x, y) {
-  x = x * 500;
-  y = canvas.height - y * 500;
-  ctx.drawImage(img, x - r, y - r, 2 * r, 2 * r);
+    x = x * 250;
+    y = canvas.height - y * 250;
+    ctx.drawImage(img, x - r, y - r, 2 * r, 2 * r);
 }
 
 function apontaRobo(gr, img, r, x, y) {
-  x = x * 500;
-  y = canvas.height - y * 500;
-  resetCanvas();
-  ctx.save();
-  ctx.translate(x, y);
-  ctx.rotate((gr * Math.PI) / 180);
-  ctx.drawImage(img, -r, -r, 2 * r, 2 * r);
-  ctx.restore();
+    x = x * 250;
+    y = canvas.height - y * 250;
+    resetCanvas();
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate((gr * Math.PI) / 180);
+    ctx.drawImage(img, -r, -r, 2 * r, 2 * r);
+    ctx.restore();
 }
 
 function desenhaLinha([xa, ya], [xb, yb]) {
-  xa = xa * 500;
-  xb = xb * 500;
-  ya = canvas.height - ya * 500;
-  yb = canvas.height - yb * 500;
-  ctx.beginPath();
-  ctx.strokeStyle = "#ff0000";
-  ctx.lineWidth = 10;
-  ctx.moveTo(xa, ya);
-  ctx.lineTo(xb, yb);
-  ctx.stroke();
-  ctx.closePath();
+    xa = xa * 250;
+    xb = xb * 250;
+    ya = canvas.height - ya * 250;
+    yb = canvas.height - yb * 250;
+    ctx.beginPath();
+    ctx.strokeStyle = "#ff0000";
+    ctx.lineWidth = 10;
+    ctx.moveTo(xa, ya);
+    ctx.lineTo(xb, yb);
+    ctx.stroke();
+    ctx.closePath();
 }
